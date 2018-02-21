5168a13e66287c05505cfec3a57ca0bf4c52040a
caglarpanus <panuscaglar@gmail.com>
2018-02-21 15:44:48 -0500

    README added

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Constructor-Hangman
-A hangman command-line game using constructor functions.
+A hangman command-line game using constructor functions. This app is ran via Node JS, and power by using JavaScript constructors.
+
+## Get Started
+* Clone Repo
+* Run command in Terminal or Gitbash 'npm install'.
+* Run command 'node index.js' to get started playing game.
+
+## Technologies Used
+
+* Node JS
+* Inquirer NPM Package - https://www.npmjs.com/package/inquirer
+* Prompt NPM Package - https://www.npmjs.com/package/prompt
+
+## Built With
+
+* VS Code 
+
--- index.js
+++ index.js
@@ -3,59 +3,10 @@ var prompt = require("prompt")
 
 var Word = require('./word.js');
 var wordSelection = ["lighthouse","volcano","pinecone","stove","beach","kayak","penguin","chimney"];
+var guessLeft= 5;
+ //Empty Array. This will hold letters that have been guessed already.
+ var guessedLetters = [];
+ var display=0;
+ var currentWord;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var word = require("./Word.js");
-// var inquirer = require("inquirer");
-// 
-// var letter= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// var guesses= [];
-// var computerPick;
-
-// function random(){
-//     var randomSelection = wordSelection[Math.floor(Math.random() * wordSelection.length)];
-//     computerPick = new word(randomSelection);
-//     computerPick.createLetters();
-// };
-
-// function showArray(){
-//     var value = computerPick.returnString();
-//     console.log(value + "\n");
-// }
-
-// random();
-// showArray();
-
-// inquirer.prompt([
-//     {
-//         type: "input",
-//         name:"play",
-//         message:"Guess A Letter"
-//     }
-// ]).then(function(answer){
-
-//     var guess = answer.play.toLowerCase();
-
-//     if(letter.indexOf(answer.play) >= 0 ){
-//         if(guesses.indexOf(answer.play)< 0){
-//             //search on google to printing colorful text in node js.
-//             console.log('\x1b[36m%s\x1b[0m', '\nCORRECT!');
-//             computerPick.wordCheck();
-//         }
-//         else{
-//             console.log('\x1b[31m%s\x1b[0m', '\nINCORRECT!');
-//         }
-//     }
-// })
+ function 
